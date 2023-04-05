ed0886c17327a07080697085e90883e26bbe1dbc
akash patel <akash.vinay.patel@gmail.com>
2023-04-05 11:24:15 -0700

    basic dark section layout

--- client/components/Background.jsx
+++ client/components/Background.jsx
@@ -4,6 +4,10 @@ import LogoMain from "../../images/Logo_Main.png"
 import LogoShield from "../../images/Logo_Shield.png"
 import Rope from "../../images/Rope_Crop.png"
 import Rainforest from "../../images/Rainforest.png"
+import Fire from "../../images/Fire_Icon.png"
+import Ice from "../../images/Ice_Icon.png"
+import Water from "../../images/Water_Icon.png"
+import Leaf from "../../images/Leaf_Icon.png"
 import Navbar from "./Navbar";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
@@ -66,10 +70,26 @@ const Background = () => {
         </div>
       </ParallaxLayer>
 
-      <ParallaxLayer sticky={{start: 2.7, end:3.7}} speed={0} 
+      <ParallaxLayer offset={2.7} speed={0} factor={2}
       style={{backgroundColor: 'black'}}
       id='black-section'>
-        <h1>h1 test</h1>
+        <h1>Phase 1: Send Elements</h1>
+        <p>Send Elements is a collection of 10k unique Elementalist Avatars on the Ethereum Blockchain</p>
+        <div id='icon-row'>
+          <p>Each individual element hosts a rare unique resource that fills the consumer with immense power and breathtaking euphoria, leading them into a state of Full Send</p>
+          <img src={Leaf}></img>
+          <img src={Water}></img>
+          <img src={Ice}></img>
+          <div></div>
+        </div>
+        <div id='avatar-row'>
+
+        </div>
+        <p>The 3 Genesis Elements are currently at in a war of control over their previous resources against the attacking Fire Element</p>
+        <img id='fire' src={Fire}>
+
+        </img>
+        <p>Why are the most beautiful things always the most destructive...</p>
       </ParallaxLayer>
       
       <ParallaxLayer sticky={{start: 2.7, end:4.7}} speed={0} factor={2}
