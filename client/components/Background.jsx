3f8c0703fbcaa38c59e35539dbcc814ceb119fc3
akash patel <akash.vinay.patel@gmail.com>
2023-04-04 20:28:51 -0700

    all layers outlined

--- client/components/Background.jsx
+++ client/components/Background.jsx
@@ -3,6 +3,7 @@ import Ocean from "../../images/Ocean.png"
 import LogoMain from "../../images/Logo_Main.png"
 import LogoShield from "../../images/Logo_Shield.png"
 import Rope from "../../images/Rope_Crop.png"
+import Rainforest from "../../images/Rainforest.png"
 import Navbar from "./Navbar";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
@@ -11,7 +12,7 @@ const Background = () => {
   const mainRef = useRef();
 
   return (
-    <Parallax id="p-container" pages={8} ref={mainRef}>
+    <Parallax id="p-container" pages={7.3} ref={mainRef}>
       <ParallaxLayer offset={0} speed={1} sticky={{start: 0, end: 1.4}}
       style={{height: '60px', overflow: 'hidden'}}
       id='nav-container'>
@@ -66,11 +67,12 @@ const Background = () => {
       </ParallaxLayer>
 
       <ParallaxLayer sticky={{start: 2.7, end:3.7}} speed={0} 
-      style={{backgroundColor: 'blue'}}>
-        <title>Inner text for Blue Test</title>
+      style={{backgroundColor: 'black'}}
+      id='black-section'>
+        <h1>h1 test</h1>
       </ParallaxLayer>
       
-      <ParallaxLayer sticky={{start: 2.7, end:4.7}} speed={0} 
+      <ParallaxLayer sticky={{start: 2.7, end:4.7}} speed={0} factor={2}
       style={{}}>
         <img src={Rope} style={{width: '100vw', top: '-15px', position: 'relative'}}></img>
       </ParallaxLayer>
@@ -80,7 +82,47 @@ const Background = () => {
         <img src={Rope} style={{width: '100vw', top: '-15px', position: 'relative'}}></img>
       </ParallaxLayer>
 
+      <ParallaxLayer offset={4.7} speed={0} factor={1.3} 
+      style={{backgroundImage: `url(${Rainforest})`, backgroundSize: '100% 100%', overflow: 'hidden'}}
+      id='rainforest'>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={4.7} speed={.4} 
+      style={{}}
+      id='rainforest-text'>
+        <div id='rf-inner'>
+          <div id='logo'>
+            <h1>ENS Governance</h1>
+            <img src={LogoShield} style={{width: '19%'}}></img>
+          </div>
+          <div id='text'>
+            <p>
+              We here at DCLA hold the keys 
+              to fullsend.eth, nelk.eth, and 
+              stevewilldoit.eth.
+            </p>
+            <p>
+              We’re aiming to use these assets 
+              as tools to empower the SC and 
+              Metacard Community while furthering 
+              the FS Brand. We have a very skilled 
+              team of designers and artists and 
+              we've been active members of the 
+              Web 3 community for over 2 years now.
+            </p>
+          </div>
+        </div>
+      </ParallaxLayer>
       
+      <ParallaxLayer offset={6} speed={0}
+      style={{backgroundColor: "black"}}
+      id='none'>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={7} speed={0} factor={.3}
+      style={{backgroundColor: "white"}}
+      id='none2'>
+      </ParallaxLayer>
 
     </Parallax>
   );
